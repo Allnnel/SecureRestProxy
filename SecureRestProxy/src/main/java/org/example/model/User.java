6f9b1eb3b680b90c50f55cfae7f325165f911ec0
Rhogoro Norine <rhogoron@un-l5.kzn.21-school.ru>
2024-03-13 17:43:09 +0300

    we don't predefine hashCode because the key is a string and not our class, methods removed

--- SecureRestProxy/src/main/java/org/example/model/User.java
+++ SecureRestProxy/src/main/java/org/example/model/User.java
@@ -53,25 +53,6 @@ public class User {
     this.company = company;
   }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    User user = (User) o;
-    return Objects.equals(name, user.name)
-        && Objects.equals(username, user.username)
-        && Objects.equals(email, user.email)
-        && Objects.equals(address, user.address)
-        && Objects.equals(phone, user.phone)
-        && Objects.equals(website, user.website)
-        && Objects.equals(company, user.company);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(username);
-  }
-
   @Getter
   @Setter
   @Embeddable
